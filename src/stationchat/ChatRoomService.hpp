49bbe894462c241c4a30195c4a43bdc00992ca61
polsommer <patricker@live.dk>
2024-06-08 19:02:35 +0200

    test

--- src/stationchat/ChatRoomService.hpp
+++ src/stationchat/ChatRoomService.hpp
@@ -3,36 +3,35 @@
 
 #include "ChatEnums.hpp"
 #include "ChatRoom.hpp"
+#include "ChatAvatarService.hpp"
+#include "mysql_driver.h"
+#include "mysql_connection.h"
+#include "cppconn/prepared_statement.h"
 
-#include <boost/optional.hpp>
-
-#include <cstdint>
 #include <memory>
 #include <set>
+#include <string>
+#include <unordered_map>
 #include <vector>
-
-struct sqlite3;
-
-class ChatAvatarService;
 
 class ChatRoomService {
 public:
-    ChatRoomService(ChatAvatarService* avatarService, sqlite3* db);
+    ChatRoomService(ChatAvatarService* avatarService, sql::Connection* db);
     ~ChatRoomService();
 
     void LoadRoomsFromStorage(const std::u16string& baseAddress);
-    
-    ChatRoom* CreateRoom(const ChatAvatar* creator,
-        const std::u16string& roomName, const std::u16string& roomTopic,
-        const std::u16string& roomPassword, uint32_t roomAttributes, uint32_t maxRoomSize,
-        const std::u16string& roomAddress, const std::u16string& srcAddress);
+
+    ChatRoom* CreateRoom(const ChatAvatar* creator, const std::u16string& roomName,
+                         const std::u16string& roomTopic, const std::u16string& roomPassword,
+                         uint32_t roomAttributes, uint32_t maxRoomSize,
+                         const std::u16string& roomAddress, const std::u16string& srcAddress);
 
     void DestroyRoom(ChatRoom* room);
 
-    ChatResultCode PersistNewRoom(ChatRoom& avatar);
+    ChatResultCode PersistNewRoom(ChatRoom& room);
 
-    std::vector<ChatRoom*> GetRoomSummaries(
-        const std::u16string& startNode, const std::u16string& filter = u"");
+    std::vector<ChatRoom*> GetRoomSummaries(const std::u16string& startNode,
+                                            const std::u16string& filter = u"");
 
     bool RoomExists(const std::u16string& roomAddress) const;
     ChatRoom* GetRoom(const std::u16string& roomAddress);
@@ -53,7 +52,7 @@ private:
     void DeleteBanned(uint32_t bannedId, uint32_t roomId);
 
     uint32_t nextRoomId_ = 0;
-    std::vector<std::unique_ptr<ChatRoom>> rooms_;
+    std::unordered_map<std::u16string, std::unique_ptr<ChatRoom>> rooms_;
     ChatAvatarService* avatarService_;
-    sqlite3* db_;
+    sql::Connection* db_;
 };
